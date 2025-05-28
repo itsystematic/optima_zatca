f0e8520d9fcad6270e163eef5eda4d14de111552
yasser1248 <yasserelbana@gmail.com>
2025-05-28 14:59:46 +0300

    fix: modify the prepayment custom fields patch , --FH

--- optima_zatca/patches/v15/create_prepayment_fields.py
+++ optima_zatca/patches/v15/create_prepayment_fields.py
@@ -4,7 +4,7 @@ from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
 def execute():
     '''
-    Create custom fields for Prepayment Sales Invoice
+    Create custom fields for Prepayment Cycle in Sales Invoice
     '''
     secho("Create Prepayment Sales Invoice Fields Patch is running", fg="yellow")
 
@@ -28,6 +28,77 @@ def get_prepayment_fields():
                 "options" : "Sales Invoice Type",
                 "default" : "Normal",
                 "reqd" : 1
+            },
+            {
+                "fieldname" : "prepayments_tab",
+                "fieldtype" : "Tab Break",
+                "label" : "Prepayments",
+                "insert_after" : "Connections",
+            },
+            {
+                "fieldname" : "previous_prepayment",
+                "fieldtype" : "Link",
+                "label" : "Previous Prepayment",
+                "options": "Prepayment Invoice",
+                "insert_after" : "Prepayments",
+            },
+            {
+                "fieldname" : "adjustment_percentage",
+                "fieldtype" : "Percent",
+                "label" : "Adjustment Percentage",
+                "insert_after" : "Previous Prepayment",
+                "default" : 0
+            },
+            {
+                "fieldname" : "prepayments_details_table",
+                "fieldtype" : "Section Break",
+                "label" : "Prepayment Details Table",
+                "insert_after" : "Adjustment Percentage",
+            },
+            {
+                "fieldname" : "prepayments_invcoies",
+                "fieldtype" : "Table",
+                "label" : "Prepayments Invoices",
+                "options" : "Prepayment Details",
+                "insert_after" : "Prepayment Details Table",
+            },
+            {
+                "fieldname" : "prepayment_totals",
+                "fieldtype" : "Section Break",
+                "label" : "Prepayment Totals",
+                "insert_after" : "Prepayments Invoices",
+            },
+            {
+                "fieldname" : "total_tax_amount",
+                "fieldtype" : "Currency",
+                "label" : "Total Tax Amount",
+                "insert_after" : "Prepayment Totals",
+                "read_only" : 1,
+                "default" : 0
+            },
+            {
+                "fieldname" : "total_taxable_amount",
+                "fieldtype" : "Currency",
+                "label" : "Total Taxable Amount",
+                "insert_after" : "Total Tax Amount",
+                "read_only" : 1,
+                "default" : 0
+            },
+            {
+                "fieldname" : "total_grands",
+                "fieldtype" : "Currency",
+                "label" : "Total Grands",
+                "insert_after" : "Total Taxable Amount",
+                "read_only" : 1,
+                "default" : 0
+            },
+            {
+                "fieldname" : "prepayment_subtotal",
+                "fieldtype" : "Currency",
+                "label" : "Prepayment Subtotal",
+                "insert_after" : "Total Grands",
+                "read_only" : 1,
+                "default" : 0
             },
         ]
     }
