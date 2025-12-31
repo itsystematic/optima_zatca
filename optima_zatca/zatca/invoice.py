69487eba83feca2b8805a742c29de60f283659af
Jamil Khoja <khoja.m.jamil@gmail.com>
2025-12-31 12:47:24 +0300

    fix:get taxable_amount from base_net_total in prepayment invoice

--- optima_zatca/zatca/invoice.py
+++ optima_zatca/zatca/invoice.py
@@ -266,7 +266,7 @@ def create_prepayment_invoice(sales_invoice, uuid: str) -> None:
         # This ensures we always store amounts in company currency
         grand_total = sales_invoice.get("base_grand_total") or sales_invoice.get("grand_total")
         tax_amount = sales_invoice.get("base_total_taxes_and_charges") or sales_invoice.get("total_taxes_and_charges")
-        taxable_amount = sales_invoice.get("base_total") or sales_invoice.get("base_net_total") or sales_invoice.get("total") or sales_invoice.get("net_total")
+        taxable_amount = sales_invoice.get("base_net_total") or sales_invoice.get("net_total")
 
         # Create and insert the document
         new_prepayment = frappe.get_doc({
